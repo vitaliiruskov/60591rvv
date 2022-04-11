5eaec1cf9a7f91acaf40a0865b17a9ec979b58fa
Виталий <ruskov@edu.surgu.ru>
2022-04-11 11:27:41 +0500

    Задание 08.04.2022 - 14.04.2022

--- catalog.php
+++ catalog.php
@@ -1,12 +1,16 @@
 <?php
 require('products_db.php');
-
-$products = get_products();
-$limit = 6;
+require('dbconnect.php');
+$limit = 4;
 $page = intval($_GET['page']) ?: 1;
 $offset = ($page - 1) * $limit;
-$products_on_page = array_slice($products, $offset, $limit, true);
-$products_quantity = count($products);
+//Запрос для получения количества записей в таблице
+$result = $conn->query("SELECT count(*) AS C FROM products");
+$row = $result->fetch();
+$products_quantity = $row['C'];
+//Запрос для получения записей из таблицы
+$result = $conn->query("SELECT * FROM products LIMIT ".$limit." OFFSET ".$offset);
+
 $pages = $products_quantity / $limit;
 $pages_total = ceil($pages);
 
@@ -16,9 +20,9 @@ $day = date('j');
 $month = date('n');
 $year = date('Y');
 
-$today = "$day $months[$month] $year";
-
+$today = $day . $months[$month] . $year;
 
 require('components/header.php');
 require('components/products_list.php');
+
 require('components/footer.php');
--- product.php
+++ product.php
@@ -1,11 +1,18 @@
 <?php
-require('products_db.php');
+//require('products_db.php');
+require('dbconnect.php');
 require('components/header.php');
 
 $id = $_GET['product_id'];
 
-$title = get_product_title($id);
-$img_url = get_img_url($id);
+$result = $conn->query("SELECT * FROM products WHERE id=".$id);
+$row = $result->fetch();
+$title = $row['title'];
+$img_url = $row['img_url'];
+$price = $row['price'];
+if ($is_violet_tuesday) {
+    require('components/product_violet.php');
+}
 
 require('components/product_info.php');
 require('components/footer.php');
