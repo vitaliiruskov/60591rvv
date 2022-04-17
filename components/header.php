ae08071c826da74384017f63eab80d7527ff629c
Виталий <ruskov@edu.surgu.ru>
2022-04-17 11:12:35 +0500

    Задание 15.04.2022 - 21.04.2022

--- components/header.php
+++ components/header.php
@@ -14,8 +14,9 @@
         </a>
         <ul class="navigation-list">
           <li><a href="catalog.php">Каталог</a></li>
-          <li><a href="delivery.php">Доставка</a></li>
-          <li><a href="cart.php">Корзина</a></li>
+          <li><a href="product_add.php">Добавить продукт</a></li>
+          <li><a href="contacts.html">Контакты</a></li>
+          <li><a href="blog.html">Блог</a></li>
         </ul>
       </nav>
     </header>
